eb657cedde704f1d8e24381e917cb3e54f4205d1
SIUUU <token.siuuu@gmail.com>
2025-04-24 14:01:53 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,31 +12,31 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 5000000
       },
       includeAssets: [
-          "**/*",
+        "**/*",
       ],
       manifest: {
-        "name": "ChessCam",
-        "short_name": "ChessCam",
-        "start_url": ".",
-        "icons": [
+        name: "Checkmate Tracker - Live Mode",
+        short_name: "Live Mode",
+        start_url: "/",
+        icons: [
           {
-              "src": "/android-chrome-192x192.png",
-              "sizes": "192x192",
-              "type": "image/png"
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-              "src": "/android-chrome-512x512.png",
-              "sizes": "512x512",
-              "type": "image/png"
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ],
-        "theme_color": "#ffffff",
-        "background_color": "#ffffff",
-        "display": "standalone"
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone"
       }
     })
   ],
   build: {
-    chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
+    chunkSizeWarningLimit: 2000,
   },
 });
